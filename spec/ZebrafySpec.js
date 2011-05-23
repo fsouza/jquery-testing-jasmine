192e0f6c87c1bee2993b24b862ef014e549236e4
Francisco Souza <francisco@franciscosouza.net>
2011-05-22 21:39:32 -0300

    Refactoring specs

--- spec/ZebrafySpec.js
+++ spec/ZebrafySpec.js
@@ -1,29 +1,42 @@
 describe('Zebrafy', function () {
+
     beforeEach(function () {
-        jQuery('<table id="zebra-table"></table>').appendTo('body');
+        this.addMatchers({
+            toBeZebrafyied: function() {
+                var isZebrafyied = true;
+
+                this.actual.find("tr:even").each(function (index, tr) {
+                    isZebrafyied = $(tr).hasClass('zebrafy-odd') === false;
+                    isZebrafyied = $(tr).hasClass('zebrafy-even');
+                });
+
+                this.actual.find("tr:odd").each(function (index, tr) {
+                    isZebrafyied = $(tr).hasClass('zebrafy-odd');
+                    isZebrafyied = $(tr).hasClass('zebrafy-even') === false;
+                });
+
+                return isZebrafyied;
+            }
+        });
+    });
+
+
+    beforeEach(function () {
+        $('<table id="zebra-table" class="zebra"></table>').appendTo('body');
         for (var i=0; i < 10; i++) {
-            jQuery('<tr></tr>').append('<td></td>').append('<td></td>').append('<td></td>').appendTo('#zebra-table');
+            $('<tr></tr>').append('<td></td>').append('<td></td>').append('<td></td>').appendTo('#zebra-table');
         };
     });
 
     afterEach(function () {
-        jQuery("#zebra-table").remove();
+        $(".zebra").remove();
     });
-    
-    
-    describe('Zebrafying table', function () {
-        it('should apply classes zebrafy-odd and zebrafy-even to each other table lines', function () {
-            jQuery("#zebra-table").zebrafy();
-            jQuery("#zebra-table tr:even").each(function (index, tr) {
-                expect($(tr).hasClass('zebrafy-odd')).toBeFalsy();
-                expect($(tr).hasClass('zebrafy-even')).toBeTruthy();
-            });
 
-            jQuery("#zebra-table tr:odd").each(function (index, tr) {
-                expect($(tr).hasClass('zebrafy-odd')).toBeTruthy();
-                expect($(tr).hasClass('zebrafy-even')).toBeFalsy();
-            });
-        });
+    it('should apply classes zebrafy-odd and zebrafy-even to each other table lines', function () {
+        var table = $("#zebra-table");
+        table.zebrafy();
+        expect(table).toBeZebrafyied();
     });
+
 });
 
